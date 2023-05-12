333a78bca054fca7853fc574dcb7556f0ca5bb01
zozoreina <119047687+zozoreina@users.noreply.github.com>
2023-05-12 13:51:06 +0200

    Cambios
    Hago el botón, lo siguiente es hacer niveles, dibujar(antes de los niveles) y terminar menús e UI's

--- Assets/Scripts/LevelMechanics/ButtonOrLever.cs
+++ Assets/Scripts/LevelMechanics/ButtonOrLever.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class ButtonOrLever : MonoBehaviour
 {
     //Referencia a los objetos que el botón o palanca afecta
-    public GameObject[] objectsToAffect;
+    public GameObject objectToAffect;
+    //Referencia al Panel de Info
+    public GameObject infoPanel;
 
     //Variable para saber si el botón o palanca ha sido activado
     bool isUsed;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,32 @@ public class ButtonOrLever : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Si el jugador está lo suficientemente cerca del botón aparece la señal
+        if (Vector2.Distance(transform.position, PlayerController.sharedInstance.transform.position) < .6)
+            infoPanel.SetActive(true);
+        else infoPanel.SetActive(false);
+
+        if (Input.GetKeyDown(KeyCode.F) && infoPanel)
+        {
+            //Si el interruptor no está activado
+            if (!isUsed)
+            {
+                //Activamos el objeto
+                objectToAffect.SetActive(true);
+                //Activamos el interruptor
+                isUsed = true;
+                //Activamos la animación del botón
+
+            }
+            else
+            {
+                //Desactivamos el objeto
+                objectToAffect.SetActive(false);
+                //Cambiamos el estado del botón
+                isUsed = false;
+                //Activamos la animación del botón
+            }
+
+        }
     }
 }
